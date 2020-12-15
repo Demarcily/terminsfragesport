77eb52bf4f9f30189d27b1d12b35ade7a4cc5610
Demarcily <60388907+Demarcily@users.noreply.github.com>
2020-12-15 10:55:07 +0100

    Shell of answer checking

--- src/questions.java
+++ src/questions.java
@@ -1,7 +1,18 @@
+import java.util.*;
+import javax.swing.*;
+
 public class questions {
   public static void main(String[] args) {
-    System.out.println("Hello");
-    System.out.println("Hello x2");
 
+    Scanner in = new Scanner(System.in);
+    int answer = in.nextInt();
+    Integer QuestionAnswer = new Integer(3);
+
+    if (QuestionAnswer.equals(answer)) {
+      System.out.println("Congrats, you were correct");
+    }
+    else {
+      System.out.println("Congrats, you suck!");
+    }
   }
 }
